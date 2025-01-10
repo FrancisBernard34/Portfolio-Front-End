826f1bde9b8455d7f25736c928dcf8c067515fdc
Francis Bernard <francisbernardcontato@gmail.com>
2025-01-10 17:32:51 -0300

    add more detailed about section presentation

--- components/about.tsx
+++ components/about.tsx
@@ -11,26 +11,30 @@ export default function About() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div>
             <p className="text-xl mb-6 terminal-text">
-              Hello! I&apos;m a passionate full-stack developer with a love for
-              creating elegant solutions to complex problems.
+              Hello! I&apos;m Francis Bernard, as you might have guessed from
+              the big flashy title above. My journey in programming began in
+              2020, exploring various domains including game development and
+              mobile applications.
             </p>
             <p className="text-xl mb-6 terminal-text">
-              With experience in both front-end and back-end development, I
-              enjoy building complete applications from the ground up.
+              Along the way, I discovered my passion for web development, which
+              led me to specialize in this field. Now, I&apos;m a dedicated
+              full-stack developer, driven by the challenge of crafting elegant
+              solutions for complex problems.
+            </p>
+            <p className="text-xl mb-6 terminal-text">
+              I specialize in Next.js, TypeScript, Nest, MongoDB, and Tailwind
+              CSS to create responsive, high-performance web apps. With
+              experience in both front-end and back-end development, I enjoy
+              building complete applications from the ground up.
             </p>
             <p className="text-xl terminal-text">
-              When I&apos;m not coding, you can find me exploring new technologies,
-              contributing to open source, or sharing knowledge with the
-              developer community.
+              When I&apos;m not coding, you can find me playing guitar, reading
+              a good book, or immersing myself in video games.
             </p>
           </div>
-          <div className="aspect-square bg-[#1E1E1E]/10 flex items-center justify-center">
-            <Image
-              src="/profile.png"
-              alt="Profile"
-              width={600}
-              height={600}
-            />
+          <div className="aspect-square bg-[#1E1E1E]/10 flex items-center justify-center select-none pointer-events-none">
+            <Image src="/profile.png" alt="Profile" width={600} height={600} />
           </div>
         </div>
       </div>
