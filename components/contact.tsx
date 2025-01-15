a0330eb17602afdb5f66c5c290fce8ff2658ab96
Francis Bernard <francisbernardcontato@gmail.com>
2025-01-14 22:34:08 -0300

    improve responsiveness of the contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,9 +4,9 @@ export default function Contact() {
       id="contact"
       className="min-h-screen bg-[#D35F0C]/90 text-[#1E1E1E] py-20"
     >
-      <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold mb-12 terminal-text text-white">Contact_Me</h2>
-        <div className="bg-white/90 backdrop-blur-sm rounded-xl p-8 shadow-lg max-w-[50%] mx-auto relative overflow-hidden">
+      <div className="container mx-auto px-8 sm:px-4">
+        <h2 className="text-3xl sm:text-4xl font-bold mb-12 terminal-text text-white">Contact_Me</h2>
+        <div className="bg-white/90 backdrop-blur-sm rounded-xl p-8 shadow-lg w-full lg:max-w-[70%] xl:max-w-[50%] mx-auto relative overflow-hidden">
           {/* Floating Circles */}
           <div className="floating-circle floating-circle-1 top-[-20px] left-[-20px]" />
           <div className="floating-circle floating-circle-2 bottom-[-30px] right-[-20px]" />
@@ -17,48 +17,48 @@ export default function Contact() {
               <div>
               <label
                 htmlFor="name"
-                className="block text-xl mb-2 terminal-text text-[#1E1E1E]"
+                className="block text-base sm:text-xl mb-2 terminal-text text-[#1E1E1E]"
               >
                 Name_
               </label>
               <input
                 type="text"
                 id="name"
-                className="w-full p-4 rounded-xl bg-[#D35F0C]/30 text-[#1E1E1E] terminal-text focus:outline-none placeholder:text-[#636262]"
+                className="w-full p-4 text-sm sm:text-base rounded-xl bg-[#D35F0C]/30 text-[#1E1E1E] terminal-text focus:outline-none placeholder:text-[#636262]"
                 placeholder="Enter your name"
               />
             </div>
             <div>
               <label
                 htmlFor="email"
-                className="block text-xl mb-2 terminal-text text-[#1E1E1E]"
+                className="block text-base sm:text-xl mb-2 terminal-text text-[#1E1E1E]"
               >
                 Email_
               </label>
               <input
                 type="email"
                 id="email"
-                className="w-full p-4 rounded-xl bg-[#D35F0C]/30 text-[#1E1E1E] terminal-text focus:outline-none placeholder:text-[#636262]"
+                className="w-full p-4 text-sm sm:text-base rounded-xl bg-[#D35F0C]/30 text-[#1E1E1E] terminal-text focus:outline-none placeholder:text-[#636262]"
                 placeholder="Enter your email"
               />
             </div>
             <div>
               <label
                 htmlFor="message"
-                className="block text-xl mb-2 terminal-text text-[#1E1E1E]"
+                className="block text-base sm:text-xl mb-2 terminal-text text-[#1E1E1E]"
               >
                 Message_
               </label>
               <textarea
                 id="message"
                 rows={6}
-                className="w-full p-4 rounded-xl bg-[#D35F0C]/30 text-[#1E1E1E] terminal-text focus:outline-none placeholder:text-[#636262]"
+                className="w-full p-4 text-sm sm:text-base rounded-xl bg-[#D35F0C]/30 text-[#1E1E1E] terminal-text focus:outline-none placeholder:text-[#636262]"
                 placeholder="Enter your message"
               />
             </div>
             <button
               type="submit"
-              className="w-full p-4 bg-[#2a2a2a] text-white terminal-text hover:bg-[#1f1e1e] rounded-xl transition-colors cursor"
+              className="w-full p-4 text-sm sm:text-base bg-[#2a2a2a] text-white terminal-text hover:bg-[#1f1e1e] rounded-xl transition-colors cursor"
             >
                 Send_Message
               </button>
